d63b4b013f2ab77e40a497b9d18df346a6c3ffae
Chris Pilsworth <cpilsworth@users.noreply.github.com>
2021-09-10 15:01:58 +0100

    Added CORS headers and fastapi glitch server

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+# work out the gitch hostname
+domain = f"https://{ os.environ['PROJECT_DOMAIN'] }.glitch.me" 
 
+app = FastAPI(root_path="/", servers=[
+        {"url": domain, "description": "Glitch environment"}  
+    ])
+
+# Add CORS headers to allow calls from everywhere
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
